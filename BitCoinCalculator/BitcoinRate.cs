8ad43d17e2a8f987afdc52db9e9848890c2b4b7a
Opilane TTHK <opilane@tthk.ee>
2021-12-07 16:03:17 +0200

    BITCOIN

---
+++ BitCoinCalculator/BitcoinRate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitCoinCalculator
+{
+    public class BitcoinRate
+    {
+        public bpi bpi { get; set; }
+    }
+    public class bpi
+    {
+        public EUR EUR { get; set; }
+        public Dollar Dollar { get; set; }
+        public GBP GBP { get; set; }
+    }
+    public class EUR
+    {
+        public string code { get; set; }
+
+        public float rate_float { get; set; }
+    }
+    public class Dollar
+    { 
+        public string code { get; set; }
+
+        public float rate_float { get; set; }
+
+    }
+    public class GBP
+    {
+        public string code { get; set; }
+
+        public float rate_float { get; set; }
+    }
+}
